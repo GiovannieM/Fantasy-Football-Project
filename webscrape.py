503014c8a1223703af788ba620a03b61374cc38c
Giovannie <moncivaisg@gmail.com>
2021-12-08 16:11:01 -0600

    save file update

--- webscrape.py
+++ webscrape.py
@@ -1,17 +1,9 @@
-#########################################################################
-
-# THIS PROGRAM IS FOR TESTING WEB SCRAPING FOR THE MASTER CODE
-
+# THIS PROGRAM IS FOR TESTING WEB SCRAPING
 #########################################################################
 from bs4 import BeautifulSoup
 import pandas as pd
-import re
 import requests
-import responses
-import validators
-
-# FFTODAY WEBSITE
-################ BS4 ####################################################
+#########################################################################
 print('Which Player would you like to get stats for? ')
 last_name = input('Enter by Last Name: ')
 print('------------------------------')
@@ -19,10 +11,11 @@ player_url = fr"https://www.fftoday.com/stats/players?Search={last_name}"
 page = requests.get(player_url).text
 soup = BeautifulSoup(page, "html.parser")
 dfs = pd.read_html(player_url)
-#########################################################################
 new_url = player_url
 response = requests.get(new_url)
-
+#########################################################################
+# CHECKS TO SEE IF MULTIPLE PLAYERS EXISTS WITH THE SAME LAST NAME
+# IF NOT THE IT CONTINUES TO SPECIFC PLAYER YEARLY STATS
 #########################################################################
 if response.history:
     print('------------------------------')
@@ -31,10 +24,13 @@ if response.history:
     df = dfs[7]
     print(df)
     print()
-    
+#########################################################################    
+# IF MULTIPLE PLAYERS EXISTS, A LIST WILL GENERATE THE LIST OF PLAYERS
+# WITH THE SAME NAME 
+#########################################################################    
 else:
-    player_names = []
-    player_list = []
+    player_names = []           # LIST CREATED FOR USER TO SELECT FROM 
+    player_list = []            # LIST OF PLAYERS TO BE PRINTED OUT 
     index = 0
     links = soup.find_all("a", href=True)
     players = soup.find_all(class_="smallbody")
@@ -47,31 +43,40 @@ else:
         index += 1
     print('------------------------------')
 #########################################################################
+# LETS USER SELECT FROM THE LIST OF GENERATED PLAYERS WITH THE SAME 
+# LAST NAME AND LETS USER CHOOSE WHICH PLAYER THEY ARE LOOKING FOR 
+#########################################################################
     select_player = 0
     while True:
         try:
-            select_player = int(input('Select specific player with corresponding number on list: '))
-            if select_player > (len(player_names)):
+            select_player = int(input('Select specific player with corresponding number on list: '))        # CHECK USER INPUT FOR A REAL NUMBER AND 
+            if select_player > (len(player_names)):                                                         # NUMBER WITHIN LIST
                 print ('Sorry that is out of range. Please Try again')
                 continue
         except ValueError:
             print("Must be an integer!")
             continue
         else:
-            print('Current yearly stats for '+player_names[select_player]+' are:')
-            sp_url = fr"https://www.fftoday.com{player_list[select_player]}"
+            print('Current yearly stats for '+player_names[select_player]+' are:')                      # PRINTS OUT SPECIFIC PLAYER CHOSEN FORM LIST 
+            sp_url = fr"https://www.fftoday.com{player_list[select_player]}"                            # AND PRINTS OUT YEARS STATS FOR THAT PLAYER
             dfs = pd.read_html(sp_url)
             print('------------------------------------------------------------')
             df = dfs[7]
             print(df)
             print('------------------------------------------------------------')
             break 
-        
-    
-    #########################################################################
-    # PRINTS THE PLAYER DATA INTO A CSV FILE
-    # df.to_excel(r"C:\Users\Giovannie\Desktop\dataproject\statscrape.xlsx")
-    # df.to_csv(r"C:\Users\Giovannie\Desktop\dataproject\tablescrape.csv")
-    # print('Table Data has been written to an excel file')
+#########################################################################
+# SAVING PLAYER STATS CAN BE OMMTTED OR SKIPPED !!
+#########################################################################
+# SAVES THE PLAYER DATA INTO A EXCEL OR CSV FILE
+# while True:
+#     if input('Would you like to create a txt fo csv file? Enter y/n : ') != 'y':
+#         break
+#     else:
+#         # PLEASE CHOOSE WHICH FILE TYPE YOU'D LIKE TO USE 
+#         df.to_excel(fr"( ~ YOUR PATH HERE ~ )\{player_names[select_player]}.xlsx")
+#         df.to_csv(fr"( ~ YOUR PATH HERE ~ )\{player_names[select_player]}.csv") 
+#         break
 
-    print('END')
+#########################################################################
+print('PLAYER STAT SCRAPING ENDED...')
